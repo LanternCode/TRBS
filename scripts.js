181882faa20e3d62365e263952b065827756f1bd
LanternCode <adam.machowczyk@gmail.com>
2022-08-14 21:20:38 +0100

    Attacking now reduces the target's health

--- scripts.js
+++ scripts.js
@@ -1,27 +1,66 @@
+//pre-defined players array
+let players = [
+    {
+        name: 'Miles',
+        health: 100,
+        speed: 81,
+        atk: 20,
+        uuid: 0
+    },
+    {
+        name: 'Churchie',
+        health: 90,
+        speed: 82,
+        atk: 15,
+        uuid: 0
+    },
+    {
+        name: 'Crownsnek',
+        health: 80,
+        speed: 17,
+        atk: 10,
+        uuid: 0
+    }];
+
+//pre-defined enemies array
+let enemies = [
+    {
+        name: 'Przeciwnik 1',
+        health: 100,
+        speed: 88,
+        atk: 20,
+        uuid: 0
+    },
+    {
+        name: 'Przeciwnik 2',
+        health: 100,
+        speed: 12,
+        atk: 20,
+        uuid: 0
+    },
+    {
+        name: 'Przeciwnik 3',
+        health: 100,
+        speed: 36,
+        atk: 20,
+        uuid: 0
+    }];
+
+//pre-defined, empty participants array
+let participants = [];
+
+//pre-defined counters
+let globalTurn = 0;
+let localTurn = 0;
+
 function startBattle()
 {
-    let players = [
-        {
-           playerName: 'Miles',
-           health: 100,
-           speed: 81,
-           atk: 20
-        },
-        {
-           playerName: 'Churchie',
-           health: 90,
-           speed: 82,
-           atk: 15
-        },
-        {
-           playerName: 'Crownsnek',
-           health: 80,
-           speed: 17,
-           atk: 10
-       }];
+    //update the array with details of players and enemies so the originals are not modified
+    participants = [...players, ...enemies];
 
-    players.sort((a, b) => b.speed - a.speed);
-    console.log(players);
+    //sort the array by speed to establish turn order
+    participants.sort((a, b) => b.speed - a.speed);
+    console.log(participants);
 
     //Update the battle state description
     document.getElementById("battleStatus").innerText = "W trakcie!";
@@ -30,19 +69,56 @@ function startBattle()
     document.getElementById("startBattleButton").style.display = "none";
     document.getElementById("nextTurnButton").style.display = "block";
 
+    //Update the "acts now" label
+    document.getElementById("nowActsDesc").innerText = participants[0].name;
+
+    //Prepare the targets list
+    for(let i = 0; i < participants.length; ++i)
+    {
+        participants[i].uuid = i;
+        let targetSlot = document.getElementById("targetsList").children[i];
+        targetSlot.innerText = participants[i].name;
+        targetSlot.value = i;
+    }
+
+    refreshBattleSlots();
+}
+
+function refreshBattleSlots()
+{
+    //Prepare the player battle slots
+    for(let i = 0; i < players.length; ++i)
+    {
+        let battleSlot = document.getElementById("participant-"+i);
+        battleSlot.children[2].innerText = players[i].health;
+        battleSlot.children[4].innerText = players[i].speed;
+        battleSlot.children[6].innerText = players[i].atk;
+    }
+
+    //Prepare the enemy battle slots
+    for(let i = 0; i < enemies.length; ++i)
+    {
+        let battleSlot = document.getElementById("participant-"+(i+3));
+        battleSlot.children[2].innerText = enemies[i].health;
+        battleSlot.children[4].innerText = enemies[i].speed;
+        battleSlot.children[6].innerText = enemies[i].atk;
+    }
 }
 
 function act()
 {
     let action = document.getElementById("action").value;
-    let target = document.getElementById("target").value;
+    let target = document.getElementById("targetsList").value;
     switch(action)
     {
         case "attack":
         {
-            let currentHealth = document.getElementById(target).children[2].value;
-            let playerAttack = document.getElementById("player-1").children[4].value;
-            document.getElementById(target).children[2].innerText = currentHealth - playerAttack;
+            let attack = participants[localTurn].atk;
+            participants[target].health -= attack;
+            break;
+        }
+        case "dodge":
+        {
             break;
         }
         default:
@@ -50,4 +126,5 @@ function act()
             break;
         }
     }
+    refreshBattleSlots();
 }
