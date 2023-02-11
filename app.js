03e369b1ed13825e7dfbdc9c4ed6fd4de567da7f
japko-7 <filipjablonski@icloud.com>
2023-02-11 13:15:09 +0100

    api updates

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ await migrateAll();
 let mc = await connectToCluster();
 
 app.get('/players/', async (req, res, next) => {
-    const players = await mc.db("TRBS").collection("player").find().toArray();
+    const players = await mc.db("TRBS").collection("player").find({}, {projection:{ _id: 0 }}).toArray();
     res.send(players);
 });
 
@@ -24,44 +24,46 @@ app.put('/participants/:participant/:type', async (req, res, next) => {
 });
 
 app.get('/enemies/', async (req, res, next) => {
-    const enemies = await mc.db("TRBS").collection("enemy").find().toArray();
+    const enemies = await mc.db("TRBS").collection("enemy").find({}, {projection:{ _id: 0 }}).toArray();
     res.send(enemies);
 });
 
 app.get('/items/', async (req, res, next) => {
-    const items = await mc.db("TRBS").collection("item").find().toArray();
+    const items = await mc.db("TRBS").collection("item").find(({}, {projection:{ _id: 0 }})).toArray();
     res.send(items);
 });
 
 app.get('/skills/', async (req, res, next) => {
-    const skills = await mc.db("TRBS").collection("skill").find().toArray();
+    const skills = await mc.db("TRBS").collection("skill").find(({}, {projection:{ _id: 0 }})).toArray();
     res.send(skills);
 });
 
 app.put('/dropParticipant/:participant/:type', async (req, res, next) => {
     if(req.params.type === "player") {
-        await mc.db("TRBS").collection(req.params.type).deleteOne({ "UID" : JSON.parse(req.params.participant.UID) });
+        await mc.db("TRBS").collection(req.params.type).deleteOne({ "UID" : JSON.parse(req.params.participant).UID });
     }
     else {
-        await mc.db("TRBS").collection(req.params.type).deleteOne({ "name" : JSON.parse(req.params.participant.name) });
+        await mc.db("TRBS").collection(req.params.type).deleteOne({ "name" : JSON.parse(req.params.participant).name });
     }
     res.sendStatus("200");
 });
 
 app.put('/updateParticipant/:participant/:type', async (req, res, next) => {
+    let participant = JSON.parse(req.params.participant);
+
     if(req.params.type === "player") {
-        await mc.db("TRBS").collection(req.params.type).updateOne({ "UID" : JSON.parse(req.params.participant.UID) }, participant);
+        await mc.db("TRBS").collection(req.params.type).replaceOne({ "UID" : participant.UID }, participant);
     }
     else {
-        await mc.db("TRBS").collection(req.params.type).updateOne({ "name" : JSON.parse(req.params.participant.name) }, participant);
+        await mc.db("TRBS").collection(req.params.type).replaceOne({ "name" : participant.name }, participant);
     }
     res.sendStatus("200");
 });
 
 app.put('/grantExperience/:player', async (req, res, next) => {
     await mc.db("TRBS").collection("player").updateOne(
-        { "UID" : JSON.parse(req.params.player.UID) },
-        { "experience": JSON.parse(req.params.player.experience) + 1 }
+        { "UID" : JSON.parse(req.params.player).UID },
+        { $set: {"experience": (parseInt(JSON.parse(req.params.player).experience) + 1)} }
     );
     res.sendStatus("200");
 });
